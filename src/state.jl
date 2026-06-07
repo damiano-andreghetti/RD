16991a044657a29160c0ca1531aa2f63694a9fa1
Alfredo Braunstein <alfredo.braunstein@polito.it>
2026-06-07 09:12:01 +0200

    use FixedSizeArrays

--- src/state.jl
+++ src/state.jl
@@ -5,7 +5,7 @@ end
 
 function State(M::Model, totmembrane::Vector, cytosol::Vector; rng = Random.default_rng())
     Nspecies, Nsites = nspecies(M), nsites(M)
-    membrane = zeros(Nspecies, Nsites)'
+    membrane = fill!(FixedSizeMatrixDefault{Int}(undef, Nspecies, Nsites), 0) |> transpose
     for m in 1:Nspecies
         for _ in 1:totmembrane[m]
             membrane[rand(rng, 1:Nsites), m] += 1
